1a8acfe92b7fcaf79aa5e7499a1ef31511472695
Viktor Blaskovics <viktor.blaskovics@gmail.com>
2022-05-23 16:30:31 +0200

    Small changes

--- app.js
+++ app.js
@@ -1,59 +1,50 @@
-const elTree = document.getElementById('tree-component')
-
 const nodeTemplate = (...args) => `
-    <div class="tree-item">
+    <div class='tree-item'>
         <p>${args[0]} ${args[1]}</p>
     </div>
 `
 
-let data = {
+
+let serverData = {
     label: 'Mappa1',
     items: [
+        { label: 'Fájl1' },
         {
-            label: "Fájl1"
-        },
-        {
-            label: "Mappa2",
+            label: 'Mappa2',
             items: [
-                {
-                    label: "Fájl2"
-                },
-                {
-                    label: "Fájl3"
-                }
+                { label: 'Fájl2 ' },
+                { label: 'Fájl3 ' },
             ]
         },
-        {
-            label: "Fájl4"
-        },
-        {
-            label: "Fájl5"
-        }
+        { label: 'Fájl4' }
     ]
 }
 
+function createNode(nodeData, elParent) {
+    if ( !nodeData || !nodeData.label ) return
+    const isFolder = Array.isArray(nodeData.items)
+    
+    // Init DOM element
+    let template = document.createElement('template')
+    const icon = isFolder ? '📂' : '📄'
+    template.innerHTML = nodeTemplate(icon, nodeData.label)
+    let elNode = elParent.appendChild(template.content.firstElementChild)
 
-function createNode(node, elParent) {
-    if (!node || !node.label) return
-    const isFolder = Array.isArray(node.items)
-
-    let template = document.createElement('template');
-    const icon = isFolder ? '📁' : '📄'
-    template.innerHTML = nodeTemplate(icon, node.label);
-    const elNode = elParent.appendChild(template.content.firstElementChild)
-
+    // Add click handling
     elNode.addEventListener('click', e => {
         e.stopPropagation()
         e.target.parentNode.classList.toggle('closed')
     })
 
+    // Add sub-nodes
     if (isFolder) {
-        node.items.forEach(node => {
+        nodeData.items.forEach( node => {
             createNode(node, elNode)
         })
     }
-
 }
 
-createNode(data, elTree)
+const elTree = document.getElementById('tree-component')
+createNode(serverData, elTree)
 
+console.log(serverData)
